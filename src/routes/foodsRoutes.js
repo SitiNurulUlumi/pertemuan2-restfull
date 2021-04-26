8ab9660a9a924d22dcae91cb0fb53934e9e28237
SitiNurulUlumi <nurul.ulumi45@gmail.com>
2021-04-26 15:39:20 +0700

    baruuu

--- src/routes/foodsRoutes.js
+++ src/routes/foodsRoutes.js
@@ -1,9 +1,9 @@
 const foodsRoutes = require("express").Router();
 const foodsControllers = require ("../controllers/foodsControllers");
 const foods = require("../models/foods");
+const authMiddleware = require("../helpers/authMiddleware");
 
-
-foodsRoutes.get("/", foodsControllers.getAllFoods);
+foodsRoutes.get("/", authMiddleware.checkLogin ,foodsControllers.getAllFoods);
 foodsRoutes.post("/", foodsControllers.postFoods);
 foodsRoutes.get("/:id", foodsControllers.getDataById );
 foodsRoutes.put("/:id", foodsControllers.putDataById);
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,8 @@
 const mainRoutes = require ("express").Router();
-const foods = require("../models/foods");
 const foodsRoutes = require("./foodsRoutes");
+const authRoutes = require("./authRoutes");
 
 mainRoutes.use("/foods", foodsRoutes);
+mainRoutes.use("/auth", authRoutes);
 
 module.exports=mainRoutes;
